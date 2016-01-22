87cf7161b355cbd3543d3b69d1ed85c4a0277319
tsinfeng <qflau@foxmail.com>
2016-01-22 15:35:26 +0800

    for stop feather

---
+++ EnjoyTest/EnjoyTest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EnjoyTest
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uniteMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.运行脚本RToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存日志SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.最近文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uniteMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uniteMenu
+            // 
+            this.uniteMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.uniteMenu.Location = new System.Drawing.Point(0, 0);
+            this.uniteMenu.Name = "uniteMenu";
+            this.uniteMenu.Size = new System.Drawing.Size(1008, 25);
+            this.uniteMenu.TabIndex = 0;
+            this.uniteMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.运行脚本RToolStripMenuItem,
+            this.保存日志SToolStripMenuItem,
+            this.最近文件FToolStripMenuItem,
+            this.退出XToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // 运行脚本RToolStripMenuItem
+            // 
+            this.运行脚本RToolStripMenuItem.Name = "运行脚本RToolStripMenuItem";
+            this.运行脚本RToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.运行脚本RToolStripMenuItem.Text = "打开脚本(&R)";
+            this.运行脚本RToolStripMenuItem.Click += new System.EventHandler(this.打开脚本RToolStripMenuItem_Click);
+            // 
+            // 保存日志SToolStripMenuItem
+            // 
+            this.保存日志SToolStripMenuItem.Name = "保存日志SToolStripMenuItem";
+            this.保存日志SToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.保存日志SToolStripMenuItem.Text = "保存日志(&S)";
+            this.保存日志SToolStripMenuItem.Click += new System.EventHandler(this.保存日志SToolStripMenuItem_Click);
+            // 
+            // 最近文件FToolStripMenuItem
+            // 
+            this.最近文件FToolStripMenuItem.Name = "最近文件FToolStripMenuItem";
+            this.最近文件FToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.最近文件FToolStripMenuItem.Text = "最近文件(&F)";
+            // 
+            // 退出XToolStripMenuItem
+            // 
+            this.退出XToolStripMenuItem.Name = "退出XToolStripMenuItem";
+            this.退出XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出XToolStripMenuItem.Text = "退出(X)";
+            this.退出XToolStripMenuItem.Click += new System.EventHandler(this.退出XToolStripMenuItem_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.uniteMenu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.uniteMenu;
+            this.Name = "mainForm";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.uniteMenu.ResumeLayout(false);
+            this.uniteMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip uniteMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 运行脚本RToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存日志SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 最近文件FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出XToolStripMenuItem;
+    }
+}
+
---
+++ EnjoyTest/EnjoyTest/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace EnjoyTest
+{
+    public partial class mainForm : Form
+    {
+        static string sSaveFilePath = "";
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            string filePath = System.Environment.CurrentDirectory;
+            filePath += "\\" + "Menu.ini";
+            StreamReader srReader = new StreamReader(filePath);
+            int i = this.fileToolStripMenuItem.DropDownItems.Count - 1;
+
+            while (srReader.Peek() >= 0)
+            {
+                ToolStripMenuItem tsmiItems = new ToolStripMenuItem(srReader.ReadLine());
+                this.fileToolStripMenuItem.DropDownItems.Insert(i, tsmiItems);
+                i++;
+                tsmiItems.Click += new EventHandler(tsmiItems_Click);
+            }
+            srReader.Close();
+        }
+
+        private void tsmiItems_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        private void app3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            App3 appForm3 = new App3();
+            appForm3.MdiParent = this;
+            appForm3.Show();
+            appForm3.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+        }
+         */
+
+        private void 打开脚本RToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "All files (*.*)|*.*|Lua files (*.lua)|*.lua|Bat files (*.bat)|*.bat|Python files (*.py)|*.py";
+            LinkedList<string> linklistLines = new LinkedList<string>();
+            string filePath = System.Environment.CurrentDirectory;
+            filePath += "\\" + "Menu.ini";
+
+            //push stack
+            StreamReader srReader = new StreamReader(filePath);
+            int i = 0;
+            while (srReader.Peek() >= 0)
+            {
+                //stack.Push(srReader.ReadLine());
+                linklistLines.AddLast(srReader.ReadLine());
+                i++;
+                if (i >= 5)
+                    break;
+            }
+            srReader.Close();
+      
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //threadLuaRunner = new Thread(new ParameterizedThreadStart(RunLua));
+
+                //threadLuaRunner.IsBackground = true;
+                //threadLuaRunner.Start(ofd.FileName);
+                
+                LinkedListNode<string> current = linklistLines.Find(ofd.FileName);
+                if (null != current)
+                {
+                    linklistLines.Remove(current);
+                    linklistLines.AddFirst(ofd.FileName);
+                }
+                else
+                {
+                    linklistLines.AddFirst(ofd.FileName);
+                }
+
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+                fs.SetLength(0);
+                fs.Close();
+
+                StreamWriter swWriter = new StreamWriter(filePath, true);
+                i = 0;
+                current = linklistLines.First;
+                while (i < 5 && current != null)
+                {
+                    swWriter.WriteLine(current.Value);
+                    i++;
+                    current = current.Next;
+                }
+                linklistLines.Clear();
+                swWriter.Flush();
+                swWriter.Close();
+
+                ScriptWin scriptWinForm = new ScriptWin(ofd.FileName);
+                scriptWinForm.MdiParent = this;
+                scriptWinForm.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+                scriptWinForm.Show();
+
+            }
+        }
+
+        private void 退出XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void 保存日志SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (保存日志SToolStripMenuItem.Checked == false)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "所有文件|*.*|文本文件|*.txt";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    sSaveFilePath = saveFileDialog.FileName;
+
+                }
+            }
+            else
+            {
+                sSaveFilePath = "";
+            }
+        }
+    }
+}
